74eb8b66eb5dbec3571b9002890ffd6d2bcca9ab
Michael Roose <mjroose@gmail.com>
2018-08-11 17:10:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,8 +44,8 @@ class Song
 
   def self.artist_count
     count = {}
-    self.artists.each do |artist|
-      count[artist] = @@artists.select(artist).length
+    self.artists.each do |unique_artist|
+      count[unique_artist] = @@artists.select {|artist| unique_artist == artist}.length
     end
     count
   end
