85a842298a543e0b29bffb5a50f202c469f73b8d
Michael Roose <mjroose@gmail.com>
2018-08-11 17:01:57 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,11 @@ class Song
     @@count
   end
 
+  def self.artists
+    unique_artists = []
+    @artists.each do |artist|
+      unique_artists << artist if !unique_artists.include(artist)
+    end
+    unique_artists
+  end
 end
