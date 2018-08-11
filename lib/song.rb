1e85889d96f357f67f084a6d6b427a3cc926f16a
Michael Roose <mjroose@gmail.com>
2018-08-11 17:02:53 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,7 @@ class Song
   def self.artists
     unique_artists = []
     @@artists.each do |artist|
-      unique_artists << artist if !unique_artists.include(artist)
+      unique_artists << artist if !unique_artists.include?(artist)
     end
     unique_artists
   end
