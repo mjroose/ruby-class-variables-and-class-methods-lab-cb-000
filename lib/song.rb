47441ab11b6d0936f435da53fb5f6ade9bea4d86
Michael Roose <mjroose@gmail.com>
2018-08-11 16:50:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,9 @@
 class Song
   attr_accessor :name, :artist, :genre
 
-  # initialize(name, artist, genre)
-  #   @name = name
-  #   @artist = artist
-  #   @genre = genre
-  # end
+  initialize(name, artist, genre)
+    # @name = name
+    # @artist = artist
+    # @genre = genre
+  end
 end
